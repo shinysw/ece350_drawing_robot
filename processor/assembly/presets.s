9f13ffc5db156b235732650f1fe812a09398f10b
Jose Santillan <mezqi14@icloud.com>
2023-04-25 01:24:38 -0400

    square preset working on gtkwave

--- processor/assembly/presets.s
+++ processor/assembly/presets.s
@@ -1,5 +1,4 @@
 .text
-addi $1, $0, 1
 j main2
 nop
 nop
@@ -19,31 +18,31 @@ square:
     addi $r21, $0, 1
     stall $0, $0, 10
 
-    addi $r19, $0, -1
-    addi $r21, $0, -1
+    addi $r19, $0, 0
+    addi $r21, $0, 0
 
     addi $r20, $0, 1
     addi $r22, $0, 1
     stall $0, $0, 10
 
-    addi $r20, $0, -1
-    addi $r22, $0, -1
+    addi $r20, $0, 0
+    addi $r22, $0, 0
 
     addi $r19, $0, -1
     addi $r21, $0, 1
     stall $0, $0, 10
 
-    addi $r19, $0, 1
-    addi $r21, $0, -1
+    addi $r19, $0, 0
+    addi $r21, $0, 0
 
     addi $r20, $0, -1
     addi $r22, $0, 1
     stall $0, $0, 10
-    addi $r20, $0, 1
-    addi $r22, $0, -1
+    addi $r20, $0, 0
+    addi $r22, $0, 0
 
     # Reset the input
-    addi $r8, $0, -1
+    addi $r8, $0, 0
     j		main2			# jump to main2
     
 triangle:
