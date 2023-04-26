03f006b04589178c0cb7c11d002395dd9268a9ae
shinysw <shinyswampert123@gmail.com>
2023-04-26 00:10:55 -0400

    Ooops staged missing files

--- processor/move_one_step.v
+++ processor/move_one_step.v
@@ -6,14 +6,22 @@ input[31:0] speed;
 output reg out; // output clock after dividing the input clock by divisor
 reg[27:0] counter = 28'd0;
 
-wire [50:0] clock_cycles;
+reg [50:0] clock_cycles;
 
-assign clock_cycles = (10 / (speed) * 100000000); 
+//assign clock_cycles = (10 / (speed) * 100000000); 
+
+reg off_flag;
+
 
 parameter DIVISOR = 28'd3;
 
 initial counter = 0;
 initial out = 0;
+
+initial clock_cycles = 50'b0;
+
+initial off_flag = 1;
+
 // The frequency of the output clk_out
 //d480000
 //  = The frequency of the input clk_in divided by DIVISOR
@@ -22,18 +30,36 @@ initial out = 0;
 // Then the frequency of the output clk_out = 50Mhz/50.000.000 = 1Hz
 
 always @(posedge clock_in) begin
-    if(counter > clock_cycles) begin
-        counter <= 28'd0;
-        //clock_out <= (counter< DIVISOR / 2) ? 1'b1 : 1'b0;
-    end
-    //else if (counter > 28'd1000) begin
-    else if (counter > 28'd1000) begin
-        counter <= counter + 28'd1;
+    //clock_cycles <= (50'd10 / (speed) * 50'd100000000); 
+    //clock_cycles <= speed * 2;
+    //clock_cycles <= (50'd10 * 50'd10000000) / (speed) ; 
+
+    if (speed == 0) begin
+        off_flag <= 1;
         out <= 0;
     end
     else begin
-        counter <= counter + 28'd1;
-        out <= 1;
+        off_flag <= 0;
+        clock_cycles <= (50'd10 * 50'd10000000) / (speed) ; 
+        //clock_cycles <= (50'd10 * 50'd100000) / (speed) ; 
+
+    end
+    
+    if (!off_flag) begin
+        //if (counter > 32'd50000) begin
+        if(counter > clock_cycles) begin
+            counter <= 28'd0;
+            //clock_out <= (counter< DIVISOR / 2) ? 1'b1 : 1'b0;
+        end
+        //else if (counter > 28'd1000) begin
+        else if (counter > 28'd10) begin
+            counter <= counter + 28'd1;
+            out <= 0;
+        end
+        else begin
+            counter <= counter + 28'd1;
+            out <= 1;
+        end
     end
 end
 
